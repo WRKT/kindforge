0400e0ae4cef107c8b3809bab60e72020d4543d9
WRKT <winness.rakotozafy@outlook.fr>
2025-06-25 17:03:23 +0200

    feat: add ingress nginx helm chart install

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,13 +5,16 @@ source ./bootstrap.env
 
 echo "Bootstrapping Kind cluster: $CLUSTER_NAME"
 
-echo "[1/5] Creating Kind cluster"
+echo "[!] Check system requirements"
+./scripts/check-requirements.sh
+
+echo "[1/6] Creating Kind cluster"
 envsubst < cluster/config.yaml | kind create cluster --config=-
 
-echo "[2/5] Generating TLS with mkcert"
+echo "[2/6] Generating TLS with mkcert"
 ./certs/install.sh
 
-echo "[3/5] Installing cert-manager"
+echo "[3/6] Installing cert-manager"
 helm repo add jetstack https://charts.jetstack.io
 helm repo update
 helm upgrade --install cert-manager jetstack/cert-manager \
@@ -22,14 +25,22 @@ helm upgrade --install cert-manager jetstack/cert-manager \
 echo "Waiting for cert-manager CRDs to be established..."
 kubectl wait --for=condition=Available --timeout=60s deployment/cert-manager -n cert-manager || true
 
-echo "[4/5] Creating mkcert CA secret"
+echo "[4/6] Creating mkcert CA secret"
 kubectl create secret tls mkcert-ca-secret \
   --cert=certs/rootCA.pem \
   --key=certs/rootCA.pem \
   -n cert-manager \
   --dry-run=client -o yaml | kubectl apply -f -
 
-echo "[5/5] Creating ClusterIssuer"
+echo "[5/6] Creating ClusterIssuer"
 envsubst < yamls/clusterissuer.yaml.tpl | kubectl apply -f -
+
+echo "[6/6] Installing Ingress-NGINX"
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace ingress-nginx \
+  --create-namespace \
+  -f charts/ingress-nginx-values.yaml
 
 echo "[OK] Your cluster '$CLUSTER_NAME' is ready with TLS and cert-manager."
